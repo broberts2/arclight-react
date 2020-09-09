eae80836cf03f2dfc146fa8124e5eb4a3c4f1f22
broberts2 <broc@compliancemonitoringsystems.com>
2020-09-09 12:04:44 -0600

    changes

--- merge.sh
+++ merge.sh
@@ -1,6 +1,6 @@
 git pull origin master
 cd /arclight-react
 npm run build
-rm -rf /var/www/arclight-react/html/build
-mv build /var/www/arclight-react/html
+rm -rf /var/www/html/arclight-react/build
+mv build /var/www/html/arclight-react
 sudo systemctl restart httpd.service
